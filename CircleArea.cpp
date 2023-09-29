d3d5554629d8d81c2967afaeb8453f23e2881836
Harsh Jaiswal <136101726+harshjaiswalprgm@users.noreply.github.com>
2023-09-30 01:07:27 +0530

    update

---
+++ CircleArea.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+class Circle {
+private:
+  double radius;
+
+public:
+  Circle(double radius) : radius(radius) {}
+
+  double area() {
+    return 3.14 * radius * radius;
+  }
+
+  double circumference() {
+    return 2 * 3.14 * radius;
+  }
+};
+
+int main() {
+  Circle circle(5);
+
+  cout << "Area: " << circle.area() << endl;
+  cout << "Circumference: " << circle.circumference() << endl;
+
+  return 0;
+}
