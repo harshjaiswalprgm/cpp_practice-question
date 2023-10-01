5deb322f71aaa4327522f906e6312b0225c4c6a6
Harsh Jaiswal <136101726+harshjaiswalprgm@users.noreply.github.com>
2023-10-02 00:16:01 +0530

    update

---
+++ Rectangle.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+                                     //Area = Length × Breadth
+                                    // Perimeter = 2 × (Length + Breadth)
+    cout<<"Area of Rectangle is: "<<5*7<<endl;
+    cout<<"Paremeter is: "<<2*(5+7)<<endl;
+    return 0;
+}
