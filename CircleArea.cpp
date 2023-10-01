886cf372178d5d5707b2753bf71b5d424004d071
Harsh Jaiswal <136101726+harshjaiswalprgm@users.noreply.github.com>
2023-10-02 00:32:51 +0530

    update

--- CircleArea.cpp
+++ CircleArea.cpp
@@ -1,32 +1,13 @@
-#include <iostream>
-
+#include<iostream>
 using namespace std;
-
-class Circle
-{
-private:
-    double radius;
-
-public:
-    Circle(double radius) : radius(radius) {}
-
-    double area()
-    {
-        return 3.14 * radius * radius;
-    }
-
-    double circumference()
-    {
-        return 2 * 3.14 * radius;
-    }
-};
-
-int main()
-{
-    Circle circle(5);
-
-    cout << "Area: " << circle.area() << endl;
-    cout << "Circumference: " << circle.circumference() << endl;
-
+int main(){
+    int radius;
+            //Area = πr²
+            //Perimeter = 2πr
+  
+    cout<<"Enter radius of circle: "<<endl;
+      cin>>radius;
+      cout<<"perimeter is: "<<2*3.14*radius<<endl;
+      cout<<"area is: "<<radius*3.14*radius<<endl;
     return 0;
 }
