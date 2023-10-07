6ae296aebc8233480f5e482f8fe01810c733fc40
Harsh Jaiswal <136101726+harshjaiswalprgm@users.noreply.github.com>
2023-10-07 23:47:56 +0530

    update

---
+++ evenodd.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    int i;
+   //for(i=2; i<=100; i+=2)  // for even number
+   //for(i=1; i<=100; i+=2)  //fro odd number
+   //for(i=100; i>=2; i-=2)   // reverse even number
+   for (i=100; i>=1; i-=2)
+    {
+        cout<<i<<endl;
+    }
+    return 0;
+}
--- table.cpp
+++ table.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main(){
     int j;
-    for(j=2;j<=20;j+=2){ 
+    for(j=2;j<=20;j+=2){ //to reverce the table just use - operator.(i-=2)
         //just change the table number which you u want.. 
         cout<<j<<endl;
     }
