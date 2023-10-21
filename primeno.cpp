9a8918b2cf1082ad7a25c2e13016209b52456262
Harsh Jaiswal <136101726+harshjaiswalprgm@users.noreply.github.com>
2023-10-21 13:40:11 +0530

    update

--- primeno.cpp
+++ primeno.cpp
@@ -1,23 +1,46 @@
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int n;
+//     int i;
+//     cout << "Enter a number :" << endl;
+//     cin >> n;
+//     for (i = 2; i < n; i++)
+//     {
+//         if (n % i == 0)
+//         {
+//             cout << "Number is not prime " << endl;
+//             break;
+//         }
+//     }
+//     if (i == n)
+//     {
+//         cout << "Number is prime" << endl;
+//     }
+
+//     return 0;
+// }
 #include <iostream>
 using namespace std;
 int main()
 {
-    int n;
-    int i;
-    cout << "Enter a number :" << endl;
+    int n, i, k;
+    cout<< "enter the nth number :" << endl;
     cin >> n;
     for (i = 2; i < n; i++)
     {
-        if (n % i == 0)
+        for (k = 2; k < i; k++)
         {
-            cout << "Number is not prime " << endl;
-            break;
+            if (i%k==0)
+            {
+                break;
+            }
+        }
+        if (k==i)
+        {
+            cout << k;
         }
     }
-    if (i == n)
-    {
-        cout << "Number is prime" << endl;
-    }
-
-    return 0;
+   
 }
