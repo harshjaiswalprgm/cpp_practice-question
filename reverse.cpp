929965152bd70eacca902d73394f893a9e7b285b
Harsh Jaiswal <136101726+harshjaiswalprgm@users.noreply.github.com>
2023-10-22 23:53:07 +0530

    update

---
+++ reverse.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    int n, r, sum = 0;
+    cout << "entre a number :" << endl;
+    cin >> n;
+    while (n != 0)
+    {
+        r = n % 10;
+        sum = sum * 10 + r;
+        n = n / 10;
+    }
+
+    cout << "recerse is :" << sum << endl;
+
+    return 0;
+}
